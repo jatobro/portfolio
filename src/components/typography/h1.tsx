4f5895718153ced1ce1b173007eeba4109e8ff2b
jatobro <jakob.tossebro@gmail.com>
2024-10-24 00:05:16 +0200

    add classname to h1

--- src/components/typography/h1.tsx
+++ src/components/typography/h1.tsx
@@ -1,8 +1,16 @@
 import { ReactNode } from "react";
 
-export function H1({ children }: { children?: ReactNode }) {
+export function H1({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <h1
+      className={`scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ${className}`}
+    >
       {children}
     </h1>
   );
